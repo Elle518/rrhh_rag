1217aa190b16e53cc69c8c6720f6cf21806c7ee4
Elle518 <teregc@hotmail.es>
2026-02-22 19:40:06 +0100

    Modify logging import

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -2,7 +2,7 @@ import logging
 import os
 from pathlib import Path
 
-from logging_utils import setup_logging
+from src.utils.logging_utils import setup_logging
 
 logger = logging.getLogger(__name__)
 
